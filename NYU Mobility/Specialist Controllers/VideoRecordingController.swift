470acdf59edfce2a5ade3a66834939c70fd2e9b7
Jin Kim <jinsungkim1210@gmail.com>
2020-07-13 20:34:46 -0700

    feat: cocoapods initialized

---
+++ NYU Mobility/Specialist Controllers/VideoRecordingController.swift
@@ -0,0 +1,12 @@
+//
+//  VideoRecordingController.swift
+//  NYU Mobility
+//
+//  Created by Jin Kim on 7/13/20.
+//  Copyright © 2020 Jin Kim. All rights reserved.
+//
+
+import UIKit
+
+// https://github.com/Kesoyuh/simple-snapchat/blob/
+// master/simple-snapchat/Controllers/CameraViewController.swift
